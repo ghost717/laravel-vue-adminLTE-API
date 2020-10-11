c5125018979665b5305431b5530c8ceaa7f730e7
ghost717 <dawid.zawadzki7717@gmail.com>
2020-10-11 12:10:03 +0200

    git clone laravel-vue-cms

---
+++ README.md
@@ -0,0 +1,21 @@
+# laravel-vue-cms
+
+
+### Build
+
+> composer create-project –prefer-dist laravel/laravel laravel-vue-cms
+
+> composer require laravel/ui
+
+> php artisan ui vue --auth
+
+> npm install
+
+> npm install --save vue-router
+
+
+### source
+
+- https://morioh.com/p/715b2bc28221
+
+- https://github.com/neoighodaro/laravel-vue-cms
---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Post;
+use Illuminate\Http\Request;
+use App\Http\Resources\PostResource;
+
+class PostController extends Controller
+{
+    public function all()
+    {
+        return view('landing', [
+            'posts' => Post::latest()->paginate(5)
+        ]);
+    }
+
+    public function single(Post $post)
+    {
+        return view('single', compact('post'));
+    }
+
+    public function index()
+    {
+        return PostResource::collection(Post::latest()->paginate(5));
+    }
+
+    public function create()
+    {
+        //
+    }
+
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'title' => 'required',
+            'body' => 'required',
+            'user_id' => 'required',
+            'image' => 'required|mimes:jpeg,png,jpg,gif,svg',
+        ]);
+
+        $post = new Post;
+
+        if ($request->hasFile('image')) {
+            $imagePath = request('image')->store('/uploads/posts', 'public');
+            // $image = $request->file('image');
+            // $name = str_slug($request->title).'.'.$image->getClientOriginalExtension();
+            // $destinationPath = public_path('/uploads/posts');
+            // $imagePath = $destinationPath . "/" . $name;
+            // $image->move($destinationPath, $name);
+            $post->image = $imagePath;
+        }
+
+        $post->user_id = $request->user_id;
+        $post->title = $request->title;
+        $post->body = $request->body;
+        $post->save();
+
+        return new PostResource($post);
+    }
+
+    public function show(Post $post)
+    {
+        return new PostResource($post);
+    }
+
+    public function edit(Post $post)
+    {
+        //
+    }
+
+    public function update(Request $request, Post $post)
+    {
+        $this->validate($request, [
+            'title' => 'required',
+            'body' => 'required',
+        ]);
+
+        $post->update($request->only(['title', 'body']));
+
+        return new PostResource($post);
+    }
+
+    public function destroy(Post $post)
+    {
+        $post->delete();
+
+        return response()->json(null, 204);
+    }
+}
